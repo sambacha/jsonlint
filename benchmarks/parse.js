f9c028608f4cd4fad0575704a953b0739e45fc63
Ferdinand Prantl <prantlf@gmail.com>
2019-05-18 18:35:52 +0200

    Add benchmark for JSON parsing with various methods

---
+++ benchmarks/parse.js
@@ -0,0 +1,27 @@
+const createSuite = require('./createSuite')
+
+const { Parser } = require('./jsonlint-pure').parser
+const { ParserWithComments } = require('../lib/jsonlint').parser
+const parser = new Parser
+const parserWithComments = new ParserWithComments
+
+const package = require('../package.json')
+const input = JSON.stringify(package, undefined, 2)
+
+function parseBuiltIn () {
+  JSON.parse(input)
+}
+
+function parseCustom () {
+  parser.parse(input)
+}
+
+function parseWithComments () {
+  parserWithComments.parse(input)
+}
+
+createSuite(`Parsing JSON string ${input.length} characters long:`)
+  .add('the built-in parser', parseBuiltIn)
+  .add('the custom parser', parseCustom)
+  .add('the parser with comment recognition', parseWithComments)
+  .start()
